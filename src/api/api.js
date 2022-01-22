130d2e906068e927e06d9a2f7691f480f6d86a7b
Gavin Everett <gavin.everett48@hotmail.com>
2022-01-22 15:24:38 +0000

    creation of URLS and GET func for newStoryIds.

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,21 @@
-//a collection of api requests
+import axios from 'axios';
 
+//creating a base url to the hackerAPI website
+export const baseURL = 'https://hacker-news.firebaseio.com/v0/';
+//creating newStories URL.
+export const newStories = `${baseURL}newstories.json`;
+//creating bestStories URL.
+export const topStories = `${baseURL}topstories.json`;
+//creating storyURL to retrieve individual stories.
+export const storyURL = `${baseURL}item.json`;
+
+
+//using axios to retrieve newStory Ids.  
+export const getNewStoryIds = async () => {
+    try {
+    const response = await axios.get(newStories).then(data => data);
+        return response;
+    } catch(error) {
+        console.log(error);
+    } 
+}
--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { getNewStoryIds } from '../../api/api';
 import { Container } from 'react-bootstrap';
 import Posts from '../Posts/Posts';
 
 
 const HomeScreen = () => {
+
+    //creation of state variable to store newStory Ids.
+    const [newStoryIds, setNewStoryIds] = useState([]);
+
+    
+    useEffect(() => {
+     
+      },[]);
+
+
     return (
         <Container fluid>
               <h1 style={{textAlign : 'center', paddingTop: '2%'}}>Todays Posts</h1>
