464227766ae629ac40575f220bb22a056203485c
Gavin Everett <gavin.everett48@hotmail.com>
2022-01-23 17:56:29 +0000

    Creating fixtures for testing.

---
+++ src/fixtures/index.js
@@ -0,0 +1,19 @@
+export const storyIds = [123];
+
+export const singleStory = {
+    id : 1,
+    by : 'Joe Bloggs',
+    url : 'http://testing.com/testing',
+    time : 1349440685,
+    title : 'Charm – tools to make the command line glamorous',
+    score : 25,
+}
+
+export const emptyStory = {
+    id : undefined,
+    by : undefined,
+    url : undefined,
+    time : undefined,
+    title : undefined,
+    score : undefined,
+}
